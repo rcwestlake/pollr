f3c61c7c2b3a0fdc422bfb0b87c8f6e5866721c5
Ryan Westlake <ryancwestlake@gmail.com>
2017-01-30 21:21:59 -0700

    [ADD]: connect Auth0, need to refactor for .env file

--- server/server.js
+++ server/server.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const bodyParser = require('body-parser')
 
 app.use(express.static('src')) //if you need static files, change this path
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.set('port', process.env.PORT || 3001)
+app.set('port', process.env.PORT || 1111)
 app.locals.title = 'Pollr' //change title if necessary
 
-app.get('/polls', (request, response) => {
-  response.sendFile('src/')
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src', 'login.html'));
 })
 
-app.get('*', (request, response) => {
-  response.send('page not found - try again')
+app.get('*', (req, res) => {
+  res.send('page not found - try again')
 })
 
 app.listen(app.get('port'), () => {
