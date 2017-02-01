ba75b11645818b16101357b66c98907b066361e4
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-01 11:44:27 -0700

    [ADD]: able to display unique poll on page

--- public/js/poll.js
+++ public/js/poll.js
@@ -1,3 +1,19 @@
+const pollQuestion = $('#poll-question')
+const pollOptions = $('#poll-options')
+let pollData;
 $(document).ready(function() {
-  console.log('in the poll.js file');
+  const uid = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
+
+  axios.get(`/api/polls/${uid}`)
+  .then(response => {
+    pollData = response.data
+    appendPollToDom(pollData)
+  })
 })
+
+const appendPollToDom = (poll) => {
+  pollQuestion.text(poll.question)
+  poll.options.map(option => {
+    pollOptions.append(`<p>${option}</p>`)
+  })
+}
