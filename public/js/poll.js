d2bc0a6dfb1d9b7911c698e6e09cdcf83d3213c3
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-01 11:49:56 -0700

    [REFACTOR]: throw error if no data, change syntax for options

--- public/js/poll.js
+++ public/js/poll.js
@@ -1,7 +1,8 @@
 const pollQuestion = $('#poll-question')
 const pollOptions = $('#poll-options')
-let pollData;
+
 $(document).ready(function() {
+  let pollData;
   const uid = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
 
   axios.get(`/api/polls/${uid}`)
@@ -12,8 +13,12 @@ $(document).ready(function() {
 })
 
 const appendPollToDom = (poll) => {
+  if(!poll.question || !poll.options.length) {
+    throw new Error('You do not have the proper info for a poll')
+  }
+
   pollQuestion.text(poll.question)
   poll.options.map(option => {
-    pollOptions.append(`<p>${option}</p>`)
+    pollOptions.append(`<li>${option}</li>`)
   })
 }
