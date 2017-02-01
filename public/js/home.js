bb8a65fbdbdddd5f4cde8949077b3c8e44725d89
Ryan Westlake <ryancwestlake@gmail.com>
2017-01-31 20:51:46 -0700

    [ADD]: link to with uid after submitting poll

--- public/js/home.js
+++ public/js/home.js
@@ -17,5 +17,11 @@ const sendPollToServer = (question, options) => {
   axios.post('/polls', { question, options })
   .then(response => {
     console.log(response);
+    appendLinkToPage(response.data)
   })
+}
+
+const appendLinkToPage = (response) => {
+  console.log('uid', response.uid);
+  $('#link').html(`<a href='/${response.uid}'>Click to share link!</a>`)
 }
