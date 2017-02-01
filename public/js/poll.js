57c3c05b704dfbd1d89758e7cb168890bd9d4d67
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-01 12:43:53 -0700

    [UPDATE]: add conditional for uid

--- public/js/poll.js
+++ public/js/poll.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
   let pollData;
   const uid = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
 
-  axios.get(`/api/polls/${uid}`)
-  .then(response => {
-    pollData = response.data
-    appendPollToDom(pollData)
-  })
+  if(uid) {
+    axios.get(`/api/polls/${uid}`)
+    .then(response => {
+      pollData = response.data
+      appendPollToDom(pollData)
+    })
+  }
 })
 
 const appendPollToDom = (poll) => {
