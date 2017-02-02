c87f8b1ca6dc3bd8994d094fb440a22a34b77b71
Ryan Westlake <ryancwestlake@gmail.com>
2017-02-01 22:30:19 -0700

    [WIP]: callbackURL is throwing error

--- public/js/poll.js
+++ public/js/poll.js
@@ -1,5 +1,6 @@
 const socket = io()
 
+const pollContainer = $('#poll-container')
 const pollQuestion = $('#poll-question')
 const pollOptions = $('#poll-options')
 const connectionCount = $('#connection-count')
@@ -8,6 +9,12 @@ const voteMessage = $('.vote-message')
 
 $(document).ready(function() {
   const id = window.location.href.substr(window.location.href.lastIndexOf('/') + 1)
+
+  axios.get('/authkeys')
+  .then(response => {
+    doAuth(response)
+  })
+
 
   getPollDataFromServer(id)
 
@@ -60,9 +67,72 @@ const appendOptionsToDom = (options) => {
       socket.emit('userVote', option.id, 'ryan')
     })
   })
-  // $('.option').on('click', function() {
-  //   console.log(this);
-  //   debugger
-  //   socket.emit('userVote', this.classList[1])
-  // })
+}
+
+/* Auth  --> remove this comment after refactor */
+
+const doAuth = (response) => {
+  var lock = new Auth0Lock(response.data.authId, response.data.authDomain, {
+    auth: {
+      redirectUrl: window.location.href
+    }
+  })
+
+  $('.btn-login').click(function(e) {
+    e.preventDefault();
+    lock.show();
+  });
+
+  $('.btn-logout').click(function(e) {
+    e.preventDefault();
+    logout();
+  })
+
+  lock.on("authenticated", function(authResult) {
+    pollContainer.show()
+    lock.getProfile(authResult.idToken, function(error, profile) {
+      if (error) {
+        return;
+      }
+      localStorage.setItem('id_token', authResult.idToken);
+      // Display user information
+      show_profile_info(profile);
+    });
+  });
+
+  //retrieve the profile:
+  var retrieve_profile = function() {
+    var id_token = localStorage.getItem('id_token');
+    if (id_token) {
+      lock.getProfile(id_token, function (err, profile) {
+        if (err) {
+          return alert('There was an error getting the profile: ' + err.message);
+        }
+        // Display user information
+        show_profile_info(profile);
+      });
+    }
+  };
+
+  function sendProfileToServer(profile) {
+    axios.post('/login', {profile: profile})
+    .then(function(response) {
+      console.log('server response', response);
+    })
+  }
+
+  var show_profile_info = function(profile) {
+     $('.nickname').text(profile.nickname);
+     $('.btn-login').hide();
+     $('.avatar').attr('src', profile.picture).show();
+     $('.btn-logout').show();
+     sendProfileToServer(profile)
+  };
+
+  var logout = function() {
+    localStorage.removeItem('id_token');
+    window.location.href = "/login";
+  };
+
+  retrieve_profile();
 }
